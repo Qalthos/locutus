0d59d89aeefb7aaa959484aec8427b7637d25435
Nathaniel Case <Qalthos@gmail.com>
2013-05-15 23:21:51 -0400

    Add remote hosts

--- site.py
+++ site.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from urllib import urlretrieve
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,6 +14,11 @@ def uptime():
     records_dict = dict()
     records = uprecord.read_file('/var/log/uptimed/records')[0]
     records_dict['localhost'] = sorted(records, key=lambda x: x[1])
+    for hostname in ['tron', 'locutus']:
+        local_copy = urlretrieve('http://' + hostname + '/uptimed/records', hostname)[0]
+        records = uprecord.read_file(local_copy)[0]
+        records_dict[hostname] = sorted(records, key=lambda x: x[1])
+
     chart = uprecord.graph_records(records_dict)
     return chart
 
