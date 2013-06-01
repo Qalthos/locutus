c6da65d37a00ab05207e56790bbf3b6abed30b56
Nathaniel Case <Qalthos@gmail.com>
2013-06-01 15:18:28 -0400

    avoid short-lived variable

--- site.py
+++ site.py
@@ -59,8 +59,7 @@ def uptime():
             records = filter(lambda x: x[1] > since, records)
         records_dict[hostname] = sorted(records, key=lambda x: x[1])
 
-    chart = uprecord.graph_records(records_dict)
-    return chart
+    return uprecord.graph_records(records_dict)
 
 
 if __name__ == '__main__':
