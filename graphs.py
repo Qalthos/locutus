98314e79501517a8e5c163c8325f93e9b29c7263
Nathaniel Case <Qalthos@gmail.com>
2013-07-31 01:06:21 -0400

    Sort hostnames by domain in ascending order of specificity

--- graphs.py
+++ graphs.py
@@ -39,7 +39,8 @@ def graph_uptime(record_files):
 def graph_records(record_files):
     chart = Bar(BaseConfig)
     max_len = max(map(len, record_files.values()))
-    for name, record_list in record_files.items():
+    for name, record_list in sorted(record_files.items(),
+                                    key=lambda x: x[0].split('.')[::-1]):
         chart.add(name, map(lambda x: x[0].total_seconds()/86400, record_list) +
                         [None] * (max_len - len(record_list)))
 
